2a487c111a4b20c9df114af8f52e0c2fee1cd161
fedfork <fakorshikov@gmail.com>
2020-05-31 23:55:56 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/LZ77.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LZ77.dir/lz77.cpp.o"
+  "LZ77.pdb"
+  "LZ77"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LZ77.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/LZ77.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LZ77.
+# This may be replaced when dependencies are built.
